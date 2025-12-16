e5bbe16dc6c0feab7039b36cc1fa29963ba57e0a
jfbarahonag <jfbarahonag@unal.edu.co>
2025-12-15 23:30:25 -0500

    adding a tickets example to understand select behavior

--- 22_select/22_main.go
+++ 22_select/22_main.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
+	// main1()
+	main2()
+}
+
+func main1() {
 	channel1 := make(chan string)
 	channel2 := make(chan string)
 	channel3 := make(chan string)
@@ -42,4 +47,42 @@ func main() {
 	}
 	finalMsg := builder.String()
 	fmt.Println("FINISH", finalMsg)
+}
+
+func main2() {
+	var remainingTickets = (int8)(5)
+
+	ch1 := make(chan int8)
+	ch2 := make(chan int8)
+	ch3 := make(chan int8)
+
+	go buyTickets(2, &remainingTickets, ch1)
+	go buyTickets(1, &remainingTickets, ch2)
+	go buyTickets(4, &remainingTickets, ch3)
+
+	for range(3) {
+		select {
+		case rem := <-ch1:
+			fmt.Println("Remaining", rem)
+		case rem := <-ch2:
+			fmt.Println("Remaining", rem)
+		case rem := <-ch3:
+			fmt.Println("Remaining", rem)
+		}
+	}
+
+}
+
+func buyTickets(n int8, remaining *int8, ch chan int8) {
+	fmt.Println("Purchasing BF", n)
+	time.Sleep(1 * time.Second)
+	fmt.Println("Purchasing AS ", n)
+	if (*remaining - n) < 0 {
+		fmt.Println("Out of stock", *remaining)
+		ch <- *remaining * -1
+		return
+	}
+	*remaining -= n
+	ch <- *remaining
+	fmt.Println("Purchase successfully")
 }
